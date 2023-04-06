e50c509b00447958d4667f9caa638ea38c6d0bbb
Daniel De Paz <vvelasquez@applaudostudios.com>
2023-04-05 21:05:40 -0600

    :sparkles: agregar clase NodoTarea

---
+++ organizador_tareas/NodoTarea.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace organizador_tareas
+{
+    class NodoTarea
+    {
+        public string nombre;
+        public DateTime fechaVencimiento;
+        public List<NodoTarea> subTareas;
+
+        public NodoTarea(string nombre, DateTime fechaVencimiento)
+        {
+            this.nombre = nombre;
+            this.fechaVencimiento = fechaVencimiento;
+            subTareas = new List<NodoTarea>();
+        }
+    }
+}
