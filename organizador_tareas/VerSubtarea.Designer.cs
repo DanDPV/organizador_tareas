998ce6424b27a6a0fad6f8a5ebe83f660e5c1b6b
Daniel De Paz <vvelasquez@applaudostudios.com>
2023-04-07 22:08:22 -0600

    :sparkles: agregar vista VerSubtarea

---
+++ organizador_tareas/VerSubtarea.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace organizador_tareas
+{
+    partial class VerSubtarea
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblNombreTarea = new System.Windows.Forms.Label();
+            this.dgvTareas = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAddTarea = new System.Windows.Forms.Button();
+            this.dtpFechaVencimiento = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTareas)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Vista subtarea";
+            // 
+            // lblNombreTarea
+            // 
+            this.lblNombreTarea.AutoSize = true;
+            this.lblNombreTarea.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreTarea.Location = new System.Drawing.Point(17, 64);
+            this.lblNombreTarea.Name = "lblNombreTarea";
+            this.lblNombreTarea.Size = new System.Drawing.Size(19, 29);
+            this.lblNombreTarea.TabIndex = 4;
+            this.lblNombreTarea.Text = ".";
+            // 
+            // dgvTareas
+            // 
+            this.dgvTareas.AllowUserToAddRows = false;
+            this.dgvTareas.AllowUserToDeleteRows = false;
+            this.dgvTareas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTareas.Location = new System.Drawing.Point(17, 113);
+            this.dgvTareas.Name = "dgvTareas";
+            this.dgvTareas.ReadOnly = true;
+            this.dgvTareas.RowHeadersWidth = 51;
+            this.dgvTareas.RowTemplate.Height = 24;
+            this.dgvTareas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvTareas.Size = new System.Drawing.Size(952, 555);
+            this.dgvTareas.TabIndex = 5;
+            this.dgvTareas.DoubleClick += new System.EventHandler(this.dgvTareas_DoubleClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAddTarea);
+            this.groupBox1.Controls.Add(this.dtpFechaVencimiento);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(976, 64);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(281, 462);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Agregar tarea";
+            // 
+            // btnAddTarea
+            // 
+            this.btnAddTarea.Location = new System.Drawing.Point(10, 196);
+            this.btnAddTarea.Name = "btnAddTarea";
+            this.btnAddTarea.Size = new System.Drawing.Size(265, 33);
+            this.btnAddTarea.TabIndex = 4;
+            this.btnAddTarea.Text = "Agregar Tarea";
+            this.btnAddTarea.UseVisualStyleBackColor = true;
+            this.btnAddTarea.Click += new System.EventHandler(this.btnAddTarea_Click);
+            // 
+            // dtpFechaVencimiento
+            // 
+            this.dtpFechaVencimiento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaVencimiento.Location = new System.Drawing.Point(10, 130);
+            this.dtpFechaVencimiento.Name = "dtpFechaVencimiento";
+            this.dtpFechaVencimiento.Size = new System.Drawing.Size(265, 22);
+            this.dtpFechaVencimiento.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Fecha vencimiento:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(10, 58);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(265, 22);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // VerSubtarea
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1269, 680);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvTareas);
+            this.Controls.Add(this.lblNombreTarea);
+            this.Controls.Add(this.label3);
+            this.Name = "VerSubtarea";
+            this.Text = "VerSubtarea";
+            this.Load += new System.EventHandler(this.VerSubtarea_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTareas)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblNombreTarea;
+        private System.Windows.Forms.DataGridView dgvTareas;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAddTarea;
+        private System.Windows.Forms.DateTimePicker dtpFechaVencimiento;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+    }
+}
